03d12c03a7872882004570e2989c6e14dde8ee62
Hi im Andre <handrelukas@gmail.com>
2024-09-17 22:11:05 -0300

    API STORE BOOKS

---
+++ API_python/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+livros = [
+    {
+        'id':1,
+        'titulo':'O senhor dos anéis',
+        'autor': 'J.R.R Tolkien'
+    },
+    
+    {
+        'id':2,
+        'titulo': '1984',
+        'autor': 'George Orwell'
+        
+    },
+    
+    {
+        'id': 3,
+        'titulo': 'Duna',
+        'autor': 'Frank Herbert'  
+    },
+    
+]
+
+# consultar(todos)
+@app.route("/livros",methods=['GET'])
+def obter_livros():
+    return jsonify(livros)
+
+#  consultar(id)
+@app.route("/livros/<int:id>",methods=['GET'])
+def obter_livro_por_id(id):
+    for livro in livros:
+       if livro.get("id") == id:
+           return jsonify(livro)
+
+# editar
+@app.route('/livros/<int:id>', methods=['PUT'])
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice,livro in enumerate(livros): #tanto indice de livros e livro
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+
+
+# criar
+@app.route("/livros", methods=['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+    
+    return jsonify(livros)
+
+# excluir
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def excluir_livros(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+            
+            return jsonify(livros)
+            
+app.run(port=5000,host="localhost",debug=True)
